8cd9e259bb59f9d747379742a0813b27e08b3ca8
Stellakoomson <stellakoomson006@gmail.com>
2024-12-16 14:04:00 +0000

    adding all changes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+function changeButtonStyle(){
+button.style.backgroundColor="blue";
+button.style.color="white";
+}
+
+
+// let button = document.querySelector("button");
+// button.addEventListener("click", changeButtonStyle);
+
+// this section opens and closes an img
+
+
+
+
+function showimage(){
+
+let image = document.querySelector(".view");
+image.style.display = "block";
+
+
+
+}
+
+let button = document.querySelector("button");
+button.addEventListener
